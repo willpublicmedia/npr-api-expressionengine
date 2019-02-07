42f52fbf6ee05c54c12f1b37d9f838c5d9c65f22
Andrew Reynolds <areynold@illinois.edu>
2019-02-07 14:59:08 -0600

    stub addon setup file

---
+++ addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+    return array(
+        'author'            => 'Illinois Public Media (WILL)',
+        'author_url'        => 'https://github.com/willpublicmedia',
+        'name'              => 'NPR Story API',
+        'description'       => "A collection of tools for publishing to and from NPR's Story API.",
+        'namespace'         => 'IllinoisPublicMedia\NprApi',
+        'version'           => '0.0.0',
+        'settings_exist'    => TRUE
+    )
+?>
