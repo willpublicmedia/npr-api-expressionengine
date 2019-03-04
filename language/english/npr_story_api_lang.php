925a30a99d36da6e89625cbbdeb436edd19707af
Andrew Reynolds <areynold@illinois.edu>
2019-03-04 15:21:46 -0600

    stub language file

---
+++ language/english/npr_story_api_lang.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+    $lang = array(
+        'npr_story_api_module_name' => 'NPR Story API',
+        'npr_story_api_module_description' => "An ExpressionEngine port of NPR's story API Wordpress module (https://github.com/npr/nprapi-wordpress).",
+        '' => ''
+    );
+?>
