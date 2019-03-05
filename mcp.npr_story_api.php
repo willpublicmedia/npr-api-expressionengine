ed9018080052af84a027795b9119ee09d7e4c01f
Andrew Reynolds <areynold@illinois.edu>
2019-03-05 09:54:32 -0600

    stub control panel server method

--- mcp.npr_story_api.php
+++ mcp.npr_story_api.php
@@ -1,5 +1,19 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed.');
-    class Npr_story_api_mcp {
-        // no mcp methods defined.
+<?php if (!defined('BASEPATH')) {
+    exit('No direct script access allowed.');
+}
+
+class Npr_story_api_mcp
+{
+    private $pull_server = '';
+
+    private $push_server = '';
+    
+    public function pull_server($server) {
+        $this->pull_server = $server;
     }
-?>
+    
+    public function push_server($server)
+    {
+        $this->push_server = $server;
+    }
+}
