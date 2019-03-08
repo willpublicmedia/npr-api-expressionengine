76732f6fe38261c8b331e3a3703deab02bd287ee
Andrew Reynolds <areynold@illinois.edu>
2019-03-08 11:43:08 -0600

    add settings view

--- mcp.npr_story_api.php
+++ mcp.npr_story_api.php
@@ -4,23 +4,35 @@
 
 class Npr_story_api_mcp
 {
-    private $pull_server = '';
+    private $api_settings = array(
+        'api_key' => '',
+        'pull_url' => '',
+        'push_url' => '',
+        'org_id' => '',
+        'npr_pull_post_type' => '',
+        'npr_push_post_type' => '',
+        'npr_permissions' => ''
+    );
 
-    private $push_server = '';
-    
-    public function pull_server($server) {
-        if ($this->validate_server) {
-            $this->pull_server = $server;
-        }
-    }
-    
-    public function push_server($server)
-    {
-        if ($this->validate_server) {
-            $this->push_server = $server;
-        }
+    private $base_url;
+
+    public function __construct() {
+        $this->base_url = ee('CP/URL')->make('addons/settings/npr_story_api');
+        ee()->load->helper('form');
     }
 
+    public function index() {
+        $values = ee()->db->get('npr_story_api_settings');
+        $settings = array(
+            'settings' => $this->api_settings,
+            'db_values' => $values
+        );
+        
+        return ee('View')->make('npr_story_api:index')->render($settings);
+    }
+
+    private $post_types = array();
+        
     private function validate_server($server) {
         return filter_var($server, FILTER_VALIDATE_URL);
     }
---
+++ views/index.php
@@ -0,0 +1,3 @@
+<?php if (!defined('BASEPATH')) { exit('No direct script access allowed'); } ?>
+
+<p>I'm a bunch of settings</p>
