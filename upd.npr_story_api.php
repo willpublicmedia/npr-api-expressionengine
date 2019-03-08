c588920d5867703c6789acfa870fb67b96072b59
Andrew Reynolds <areynold@illinois.edu>
2019-03-08 10:27:27 -0600

    create npr field mappings table

--- upd.npr_story_api.php
+++ upd.npr_story_api.php
@@ -1,92 +1,141 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-    class Npr_story_api_upd {
-        private $version = '0.0.0';
+<?php if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
 
-        private $module_name = 'Npr_story_api';
+class Npr_story_api_upd
+{
+    private $version = '0.0.0';
 
-        public function install() {
-            $data = array(
-                'module_name' => $this->module_name,
-                'module_version' => $this->version,
-                'has_cp_backend' => 'y',
-                'has_publish_fields' => 'n'
-            );
+    private $module_name = 'Npr_story_api';
 
-            ee()->db->insert('modules', $data);
+    public function install()
+    {
+        $data = array(
+            'module_name' => $this->module_name,
+            'module_version' => $this->version,
+            'has_cp_backend' => 'y',
+            'has_publish_fields' => 'n',
+        );
 
-            $this->create_settings_table();
+        ee()->db->insert('modules', $data);
 
-            return TRUE;
-        }
+        $this->create_settings_table();
 
-        public function uninstall() {
-            ee()->db->select('module_id');
-            ee()->db->from('modules');
-            ee()->db->where('module_name', $this->module_name);
-            $query = ee()->db->get();
-    
-            ee()->db->delete('module_member_groups', array('module_id' => $query->row('module_id')));
-            ee()->db->delete('modules', array('module_name' => $this->module_name));
-            ee()->db->delete('actions', array('class' => $this->module_name));
-            ee()->db->delete('actions', array('class' => 'Ipm_pledge_tracker_mcp'));
-
-            $this->delete_settings_table();
-
-		    return TRUE;
-        }
-
-        public function update($current = '') {
-            if (version_compare($current, $this->version, '='))
-            {
-                return FALSE;
-            }
-
-            return TRUE;
-        }
-
-        private function create_settings_table() {
-            ee()->load->dbforge();
-            
-            $fields = array(
-                'settings_id' => array(
-                    'type' => 'int',
-                    'constraint' => 10,
-                    'unsigned' => TRUE,
-                    'auto_increment' => TRUE
-                ),
-                'api_key' => array(
-                    'type' => 'varchar',
-                    'constraint' => 64
-                ),
-                'npr_pull_post_type' => array(
-                    'type' => 'varchar',
-                    'constraint' => 64
-                ),
-                'npr_push_post_type' => array(
-                    'type' => 'varchar',
-                    'constraint' => 64
-                ),
-                'org_id' => array(
-                    'type' => 'int',
-                    'constraint' => 10
-                ),
-                'pull_url' => array(
-                    'type' => 'varchar',
-                    'constraint' => 64
-                ),
-                'push_url' => array(
-                    'type' => 'varchar',
-                    'constraint' => 64
-                )
-            );
-            
-            ee()->dbforge->add_key('settings_id', TRUE);
-            ee()->dbforge->add_field($fields);
-            ee()->dbforge->create_table('npr_story_api_settings');
-        }
-
-        private function delete_settings_table() {
-            throw new Exception('not implemented.');
-        }
+        return true;
     }
-?>
+
+    public function uninstall()
+    {
+        ee()->db->select('module_id');
+        ee()->db->from('modules');
+        ee()->db->where('module_name', $this->module_name);
+        $query = ee()->db->get();
+
+        ee()->db->delete('module_member_groups', array('module_id' => $query->row('module_id')));
+        ee()->db->delete('modules', array('module_name' => $this->module_name));
+        ee()->db->delete('actions', array('class' => $this->module_name));
+        ee()->db->delete('actions', array('class' => 'Ipm_pledge_tracker_mcp'));
+
+        $this->delete_settings_table();
+
+        return true;
+    }
+
+    public function update($current = '')
+    {
+        if (version_compare($current, $this->version, '=')) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function create_field_mappings_table()
+    {
+        ee()->load->dbforge();
+
+        $fields = array(
+            'id' => array(
+                'type' => 'int',
+                'constraint' => 10,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ),
+            'custom_settings' => array(
+                'type' => 'boolean',
+            ),
+            'media_agency_field' => array(
+                'type' => 'varchar',
+                'constraint' => 128,
+            ),
+            'media_credit_field' => array(
+                'type' => 'varchar',
+                'constraint' => 128,
+            ),
+            'story_title' => array(
+                'type' => 'varchar',
+                'constraint' => 128,
+            ),
+            'story_body' => array(
+                'type' => 'varchar',
+                'constraint' => 128,
+            ),
+            'story_byline' => array(
+                'type' => 'varchar',
+                'constraint' => 128,
+            ),
+        );
+        ee()->dbforge->add_key('id', true);
+        ee()->dbforge->add_field($fields);
+        ee()->dbforge->create_table('npr_story_api_settings');
+    }
+    private function create_settings_table()
+    {
+        ee()->load->dbforge();
+
+        $fields = array(
+            'id' => array(
+                'type' => 'int',
+                'constraint' => 10,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ),
+            'api_key' => array(
+                'type' => 'varchar',
+                'constraint' => 64,
+            ),
+            'npr_permissions' => array(
+                'type' => 'varchar',
+            ),
+            'npr_pull_post_type' => array(
+                'type' => 'varchar',
+                'constraint' => 64,
+            ),
+            'npr_push_post_type' => array(
+                'type' => 'varchar',
+                'constraint' => 64,
+            ),
+            'org_id' => array(
+                'type' => 'int',
+                'constraint' => 10,
+            ),
+            'pull_url' => array(
+                'type' => 'varchar',
+                'constraint' => 64,
+            ),
+            'push_url' => array(
+                'type' => 'varchar',
+                'constraint' => 64,
+            ),
+        );
+
+        ee()->dbforge->add_key('id', true);
+        ee()->dbforge->add_field($fields);
+        ee()->dbforge->create_table('npr_story_api_settings');
+    }
+
+    private function delete_settings_table()
+    {
+        throw new Exception('not implemented.');
+    }
+}
