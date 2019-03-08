bdb56bb8b654e190161dab13ccbaaa4fde60eed8
Andrew Reynolds <areynold@illinois.edu>
2019-03-08 12:00:42 -0600

    loop over setting keys

--- views/index.php
+++ views/index.php
@@ -1,3 +1,12 @@
 <?php if (!defined('BASEPATH')) { exit('No direct script access allowed'); } ?>
 
 <p>I'm a bunch of settings</p>
+<p>I'm looking for</p>
+<ul>
+	<?php
+		$keys = array_keys($settings);
+		foreach ($keys as $setting) { 
+			print("<li>{$setting}</li>"); 
+		} 
+	?>
+</ul>
