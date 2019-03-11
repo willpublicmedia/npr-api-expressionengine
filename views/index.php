d8b9626c290c0addc564824323d4aa9ac80049d8
Andrew Reynolds <areynold@illinois.edu>
2019-03-11 16:51:09 -0500

    stub field mappings model

--- views/index.php
+++ views/index.php
@@ -1,14 +1,14 @@
-<?php if (!defined('BASEPATH')) { exit('No direct script access allowed'); } ?>
+<?php if (!defined('BASEPATH')) {exit('No direct script access allowed');}?>
 
 <p>I'm a bunch of settings</p>
 <p>I'm looking for</p>
 <ul>
 	<?php
-		$keys = array_keys($settings);
-		var_dump($settings);
-		foreach ($keys as $setting) {
-			$name = $settings[$setting]['display_name'];
-			print("<li>{$name}</li>"); 
-		} 
-	?>
+$keys = array_keys($settings);
+var_dump($settings);
+foreach ($keys as $setting) {
+    $name = $settings[$setting]['display_name'];
+    print("<li>{$name}</li>");
+}
+?>
 </ul>
