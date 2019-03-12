af87ef44accaae941195476103d98002fe838155
Andrew Reynolds <areynold@illinois.edu>
2019-03-12 10:07:42 -0500

    check control panel permissions

--- mcp.npr_story_api.php
+++ mcp.npr_story_api.php
@@ -38,6 +38,7 @@ class Npr_story_api_mcp
     private $base_url;
 
     public function __construct() {
+        $this->_permissions_check();
         $this->base_url = ee('CP/URL')->make('addons/settings/npr_story_api');
         ee()->load->helper('form');
     }
@@ -57,4 +58,43 @@ class Npr_story_api_mcp
     private function validate_server($server) {
         return filter_var($server, FILTER_VALIDATE_URL);
     }
+
+    /**
+	 * Makes sure users can access a given method
+	 *
+	 * @access	private
+	 * @return	void
+	 */
+	private function _permissions_check()
+	{
+		// super admins always can
+		$can_access = (ee()->session->userdata('group_id') == '1');
+
+		if ( ! $can_access)
+		{
+			// get the group_ids with access
+			$result = ee()->db->select('module_member_groups.group_id')
+				->from('module_member_groups')
+				->join('modules', 'modules.module_id = module_member_groups.module_id')
+				->where('modules.module_name',$this->name)
+				->get();
+
+			if ($result->num_rows())
+			{
+				foreach ($result->result_array() as $r)
+				{
+					if (ee()->session->userdata('group_id') == $r['group_id'])
+					{
+						$can_access = TRUE;
+						break;
+					}
+				}
+			}
+		}
+
+		if ( ! $can_access)
+		{
+			show_error(lang('unauthorized_access'), 403);
+		}
+	}
 }
