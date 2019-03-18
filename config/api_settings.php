6de6bcdd8513bf9949a0dc14e98784df8e6111ce
Andrew Reynolds <areynold@illinois.edu>
2019-03-18 14:17:31 -0500

    create config directory

---
+++ config/api_settings.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed.');
+}
+
+$api_key = '';
+$pull_url = '';
+$push_url = '';
+$org_id = '';
+$npr_pull_post_type = '';
+$npr_push_post_type = '';
+$npr_permissions = '';
