c6cfd5b14aa3f93a2e8aafab3a88b20fc7a79ac6
Andrew Reynolds <areynold@illinois.edu>
2019-03-19 16:38:25 -0500

    stub config file installer

---
+++ libraries/configuration/config_installer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace IllinoisPublicMedia\NprStoryApi\Libraries\Configuration;
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed.');
+}
+
+class Config_installer {
+    public function install() {
+        throw new Exception('not implemented');
+    }
+}
