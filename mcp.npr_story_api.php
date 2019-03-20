2c11ab11d95364ec45152755919c88c0a599b9ac
Andrew Reynolds <areynold@illinois.edu>
2019-03-20 09:18:40 -0500

    avoid config var namespace collision on load

--- mcp.npr_story_api.php
+++ mcp.npr_story_api.php
@@ -56,7 +56,7 @@ class Npr_story_api_mcp
     private function load_settings()
     {
         // $settings = ee()->config->get("npr_story_api:config.api_settings");
-        $config = ee()->config->load('npr_story_api');
+        $config = ee()->config->load('npr_story_api', TRUE);
         $settings = $config->item('api_settings');
         $this->api_settings = $settings;
     }
