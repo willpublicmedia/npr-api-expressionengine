9c07013aa6fdfd83fedc82d10c7c94f4a8e1d419
Andrew Reynolds <areynold@illinois.edu>
2019-03-20 11:16:18 -0500

    match validator settings to db

--- libraries/validation/settings_validator.php
+++ libraries/validation/settings_validator.php
@@ -9,9 +9,10 @@ if (!defined('BASEPATH')) {
 class Settings_validator {
     public const API_SETTINGS_RULES = array(
         'api_key' => 'required|maxLength[64]|alphaNumeric',
-        'pull_url' => 'url|maxLength[255]',
-        'push_url' => 'url|maxLength[255]',
-        'org_id' => 'maxLength[64]|alphaNumeric',
+        'pull_url' => 'url|maxLength[64]',
+        'push_url' => 'url|maxLength[64]',
+        'org_id' => 'maxLength[10]|alphaNumeric',
+        'npr_permissions' => 'maxLength[256]|alphaNumeric',
         'npr_pull_post_type' => 'alpha|maxLength[64]',
         'npr_push_post_type' => 'alpha|maxLength[64]'
     );
