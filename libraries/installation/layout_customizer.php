3a659f2abf532ddc0b487062c66867d2df17c8fc
Andrew Reynolds <areynold@illinois.edu>
2019-04-15 15:21:47 -0500

    create layout from channel default

--- libraries/installation/layout_customizer.php
+++ libraries/installation/layout_customizer.php
@@ -7,33 +7,17 @@ if (!defined('BASEPATH')) {
 }
 
 use EllisLab\ExpressionEngine\Model\Channel\Channel;
-use EllisLab\ExpressionEngine\Model\Channel\Display\DefaultChannelLayout;
+use IllinoisPublicMedia\NprStoryApi\Libraries\Model\Channel\Default_npr_story_layout;
 
 class Layout_customizer {
-    private $available_fields = array(
-        'entry_source' => 'add_entry_source'
-    );
-
     private $channel;
 
     public function __construct($channel) {
-        ee()->load->library('layout');
-
         $this->channel = $channel;
-    }
-
-    public function add_field($field_name) {
-        if (!array_key_exists($field_name, $this->available_fields)) {
-            throw new \Exception("No field creation method exists for {$field_name}.");
-        }
-
-        $method = $this->available_fields[$field_name];
-        $this->{$method}();
     }
 
     public function install($layout_name, $field_names) {
         $this->create_layout($layout_name);
-
         $this->assign_layout($layout_name, $this->channel);
     }
 
@@ -57,26 +41,15 @@ class Layout_customizer {
     private function create_layout($layout_name) {
         $model = ee('Model')->get('ChannelLayout')->filter('layout_name', '==', $layout_name)->first();
 
-        if ($model == null) {
-            $model = ee('Model')->make('ChannelLayout', array( 'layout_name' => $layout_name ));
+        if ($model != null) {
+            return;
         }
-        
-        $default_layout = new DefaultChannelLayout($this->channel->channel_id, NULL);
-        var_dump($default_layout);
 
-        var_dump($model);
+        $default = new Default_npr_story_layout($this->channel->channel_id, NULL);
+        $layout = $default->getLayout();
 
+        $model = ee('Model')->make('ChannelLayout', $layout);
+        $model->layout_name = $layout_name;
         $model->save();
-
-        // $model->save();
-        // $tabs[] = $tabs['options'] = array(
-        //     'entry_source' => array(
-        //         'visible'		=> 'true',
-        //         'collapse'		=> 'false',
-        //         'htmlbuttons'	=> 'true',
-        //         'width'			=> '100%'
-        //         )
-        //     );
-        // ee()->layout->add_layout_fields($tabs, array($this->channel->channel_id));
     }
 }
