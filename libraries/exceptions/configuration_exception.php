cf5b7e50ce151e904797c9913f48749726f9b451
Andrew Reynolds <areynold@illinois.edu>
2019-04-22 14:32:54 -0500

    stub config exception

---
+++ libraries/exceptions/configuration_exception.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace IllinoisPublicMedia\NprStoryApi\Libraries\Exceptions;
+
+if (!defined('BASEPATH')) {
+    exit ('No direct script access allowed.');
+}
+
+use \Exception;
+    
+class Configuration_exception extends Exception {
+}
