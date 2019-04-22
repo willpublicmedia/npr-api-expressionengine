9befa599b16a2b6854873476891cc584f08cfbf7
Andrew Reynolds <areynold@illinois.edu>
2019-04-22 10:36:58 -0500

    get npr story id from post

--- ext.npr_story_api.php
+++ ext.npr_story_api.php
@@ -49,20 +49,33 @@ class Npr_story_api_ext {
             return;
         }
 
-        print_r($_POST);
+        $npr_story_id = $this->get_npr_story_id();
+
+        print_r("npr story id: {$npr_story_id}.");
     }
 
     private function check_external_story_source() {
-        $source_field_id = ee('Model')->get('ChannelField')
+        $field_id = ee('Model')->get('ChannelField')
             ->filter('field_name', 'channel_entry_source')
             ->first()
             ->field_id;
         
-        $story_source = ee()->input->post("field_id_{$source_field_id}");
+        $story_source = ee()->input->post("field_id_{$field_id}");
         if ($story_source == NULL || $story_source == 'local') {
             return FALSE;
         }
 
         return TRUE;
+    }
+
+    private function get_npr_story_id() {
+        $field_id = ee('Model')->get('ChannelField')
+        ->filter('field_name', 'npr_story_id')
+        ->first()
+        ->field_id;
+    
+        $npr_story_id = ee()->input->post("field_id_{$field_id}", TRUE);
+
+        return $npr_story_id;
     }
 }
