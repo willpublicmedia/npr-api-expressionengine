f16bf761bebdd868a74d776731ae7b10d42b11df
Andrew Reynolds <areynold@illinois.edu>
2019-05-07 15:48:05 -0500

    stub table creation

--- libraries/installation/table_installer.php
+++ libraries/installation/table_installer.php
@@ -26,7 +26,7 @@ class Table_installer {
      */
     public function install(array $table_configs) {
         foreach ($table_configs as $table) {
-            // do something
+            $this->create_table($table);
         }
     }
 
@@ -39,5 +39,34 @@ class Table_installer {
         foreach($table_names as $table) {
             ee()->dbforge->drop_table($table);
         }
+    }
+
+    private function create_table(Table $table_config) {
+        $name = $table->table_name();
+        
+        if (!ee()->db->table_exists($name)) {
+            $keys = $table->keys();
+            $fields = $table->fields();
+
+            ee()->dbforge->add_key($keys['primary'], TRUE);
+            ee()->dbforge->add_field($fields);
+            ee()->dbforge->create_table($name);
+            ee()->db->insert($name, $fields);
+        }
+
+        $defaults = $table->defaults();
+        if (!empty($defaults)) {
+            $this->add_default_settings($name, $defaults);
+        }
+    }
+
+    private function add_default_settings(string $table_name, array $default_settings) {
+        $results = ee()->db->get($table);
+        
+        if (!empty($results->result_array())) {
+            return;
+        }
+        
+        ee()->db->insert($table, $defaults);
     }
 }
