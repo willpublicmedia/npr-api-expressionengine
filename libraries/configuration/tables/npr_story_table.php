86a8fcf034d4242a40e6ff268328ab5cb72d60e3
Andrew Reynolds <areynold@illinois.edu>
2019-05-08 11:03:32 -0500

    stub additional npr_story fields

--- libraries/configuration/tables/npr_story_table.php
+++ libraries/configuration/tables/npr_story_table.php
@@ -10,4 +10,10 @@ use IllinoisPublicMedia\NprStoryApi\Libraries\Configuration\Tables\Table;
 
 class npr_story_table extends Table {
     protected $_table_name = 'npr_story_api_stories';
+
+    protected $_defaults = array();
+    
+    protected $_fields = array();
+    
+    protected $_keys = array();
 }
