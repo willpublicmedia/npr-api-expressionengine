6c4e2842dd4b6b3e7ad6952f79d153cc5a4f623b
Andrew Reynolds <areynold@illinois.edu>
2019-05-09 09:50:23 -0500

    move config table definitions to tables namespace

---
+++ libraries/configuration/tables/config_field_mappings_table.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace IllinoisPublicMedia\NprStoryApi\Libraries\Configuration\Tables;
+
+if (!defined('BASEPATH')) {
+    exit ('No direct script access allowed.');
+}
+
+use IllinoisPublicMedia\NprStoryApi\Libraries\Configuration\Tables\Table;
+
+class config_field_mappings_table extends Table {
+    protected $_defaults = array(
+        'custom_settings' => FALSE,
+            'media_agency_field' => '',
+            'media_credit_field' => '',
+            'story_title' => '',
+            'story_body' => '',
+            'story_byline' => ''
+    );
+
+    protected $_fields = array(
+        'id' => array(
+            'type' => 'int',
+            'constraint' => 10,
+            'unsigned' => true,
+            'auto_increment' => true,
+        ),
+        'custom_settings' => array(
+            'type' => 'boolean',
+        ),
+        'media_agency_field' => array(
+            'type' => 'varchar',
+            'constraint' => 128,
+        ),
+        'media_credit_field' => array(
+            'type' => 'varchar',
+            'constraint' => 128,
+        ),
+        'story_title' => array(
+            'type' => 'varchar',
+            'constraint' => 128,
+        ),
+        'story_body' => array(
+            'type' => 'varchar',
+            'constraint' => 128,
+        ),
+        'story_byline' => array(
+            'type' => 'varchar',
+            'constraint' => 128,
+        )
+    );
+
+    protected $_keys = array(
+        'primary' => 'id'
+    );
+
+    protected $_table_name = 'npr_story_api_field_mappings';
+}
---
+++ libraries/configuration/tables/config_settings_table.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace IllinoisPublicMedia\NprStoryApi\Libraries\Configuration\Tables;
+
+if (!defined('BASEPATH')) {
+    exit ('No direct script access allowed.');
+}
+
+use IllinoisPublicMedia\NprStoryApi\Libraries\Configuration\Tables\Table;
+
+class config_settings_table extends Table {
+    protected $_defaults = array(
+        'api_key' => '',
+        'npr_permissions' => '',
+        'npr_pull_post_type' => '',
+        'npr_push_post_type' => '',
+        'org_id' => null,
+        'pull_url' => '',
+        'push_url' => ''
+    );
+    
+    protected $_fields = array(
+        'id' => array(
+            'type' => 'int',
+            'constraint' => 10,
+            'unsigned' => true,
+            'auto_increment' => true,
+        ),
+        'api_key' => array(
+            'type' => 'varchar',
+            'constraint' => 64
+        ),
+        'npr_permissions' => array(
+            'type' => 'varchar',
+            'constraint' => 256
+        ),
+        'npr_pull_post_type' => array(
+            'type' => 'varchar',
+            'constraint' => 64
+        ),
+        'npr_push_post_type' => array(
+            'type' => 'varchar',
+            'constraint' => 64
+        ),
+        'org_id' => array(
+            'type' => 'int',
+            'null' => TRUE,
+            'constraint' => 10
+        ),
+        'pull_url' => array(
+            'type' => 'varchar',
+            'constraint' => 64,
+        ),
+        'push_url' => array(
+            'type' => 'varchar',
+            'constraint' => 64
+        )
+    );
+
+    protected $_keys = array(
+        'primary' => 'id'
+    );
+
+    protected $_table_name = 'npr_story_api_settings';
+}
