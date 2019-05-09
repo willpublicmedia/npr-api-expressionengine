7f56bdc960c3b4cec27feb33d9c2081844323a99
Andrew Reynolds <areynold@illinois.edu>
2019-05-09 16:01:42 -0500

    add dbprefix before querying tables

--- libraries/installation/table_installer.php
+++ libraries/installation/table_installer.php
@@ -12,6 +12,8 @@ use IllinoisPublicMedia\NprStoryApi\Libraries\Configuration\Tables\ITable;
  * NPR Story API table installer.
  */
 class Table_installer {
+    private $prefix;
+
     /**
      * Constructor.
      *
@@ -19,6 +21,8 @@ class Table_installer {
      */
     public function __construct() {
         ee()->load->dbutil();
+
+        $this->prefix = ee()->config->item('dbprefix');
     }
 
     /**
@@ -38,10 +42,8 @@ class Table_installer {
      * @return void
      */
     public function uninstall(array $table_names) {
-        $prefix = ee()->config->item('dbprefix');
-
         foreach($table_names as $table) {
-            $table_name = $prefix . $table;
+            $table_name = $this->prefix . $table;
             ee()->dbforge->drop_table($table_name);
         }
     }
@@ -49,22 +51,24 @@ class Table_installer {
     private function create_table(ITable $table) {
         $name = $table->table_name();
         
-        if (!ee()->db->table_exists($name)) {
-            $keys = $table->keys();
-            $fields = $table->fields();
-
-            ee()->dbforge->add_field($fields);
-                        
-            ee()->dbforge->add_key($keys['primary'], TRUE);
-            if (array_key_exists('foreign', $keys)) {
-                foreach ($keys['foreign'] as $foreign_key) {
-                    ee()->dbforge->add_key($foreign_key['column']);
-                }
-            }
-
-            ee()->dbforge->create_table($name);
-            ee()->db->insert($name, $fields);
+        if (ee()->db->table_exists($name)) {
+            return;
         }
+
+        $keys = $table->keys();
+        $fields = $table->fields();
+
+        ee()->dbforge->add_field($fields);
+                    
+        ee()->dbforge->add_key($keys['primary'], TRUE);
+        if (array_key_exists('foreign', $keys)) {
+            foreach ($keys['foreign'] as $foreign_key) {
+                ee()->dbforge->add_key($foreign_key['column']);
+            }
+        }
+
+        ee()->dbforge->create_table($name);
+        ee()->db->insert($name, $fields);
 
         $defaults = $table->defaults();
         if (!empty($defaults)) {
@@ -73,12 +77,16 @@ class Table_installer {
     }
 
     private function add_default_settings(string $table_name, array $default_settings) {
-        $results = ee()->db->get($table);
+        if (!substr($table_name, 0, strlen($this->prefix)) === $this->prefix) {
+            $table_name = $this->prefix . $table_name;
+        }
+        
+        $results = ee()->db->get($table_name);
         
         if (!empty($results->result_array())) {
             return;
         }
         
-        ee()->db->insert($table, $defaults);
+        ee()->db->insert($table_name, $defaults);
     }
 }
