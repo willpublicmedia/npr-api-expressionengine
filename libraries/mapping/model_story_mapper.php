1406a726e85e24eb1b74a19436ba6dd9361dd840
Andrew Reynolds <areynold@illinois.edu>
2019-05-10 10:10:50 -0500

    stub title generation

--- libraries/mapping/model_story_mapper.php
+++ libraries/mapping/model_story_mapper.php
@@ -7,9 +7,26 @@ if (!defined('BASEPATH')) {
 }
 
 use EllisLab\ExpressionEngine\Service\Model\Model;
+use IllinoisPublicMedia\NprStoryApi\Libraries\Model\Content\Npr_story;
 
 class Model_story_mapper {
     public function map_parsed_story($story): Model {
+        $model = $this->load_base_model($story->id);
+        $model->title = $story->title->value;
+        $model->slug = $story->slug->value;
+        // move this to channel behavior
+        //$model->slug = ee('Format')->make('Text', $story->slug->value)->urlSlug(['separator' => '-', 'lowercase' => TRUE]);
         throw new \Exception('not implemented');
+    }
+
+    private function load_base_model($story_id) {
+        if (ee('Model')->get('Npr_story')->filter('id', $story_id)->count() > 0) {
+            return ee('Model')->get('Npr_story')->filter('id', $story_id)->first();
+        }    
+        
+        $model = ee('Model')->make('Npr_story');
+        $model->id = $story_id;
+
+        return $model;
     }
 }
