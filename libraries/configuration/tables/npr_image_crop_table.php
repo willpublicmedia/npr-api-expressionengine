50a49f66eda4926bf3f14dfe996a5c01cc38c0df
Andrew Reynolds <areynold@illinois.edu>
2019-07-24 10:15:00 -0500

    install image crop table

---
+++ libraries/configuration/tables/npr_image_crop_table.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace IllinoisPublicMedia\NprStoryApi\Libraries\Configuration\Tables;
+
+if (!defined('BASEPATH')) {
+    exit ('No direct script access allowed.');
+}
+
+use IllinoisPublicMedia\NprStoryApi\Libraries\Configuration\Tables\Table;
+
+class npr_image_crop_table extends Table {
+    protected $_defaults = array();
+
+    protected $_fields = array(
+        'id' => array(
+            'type' => 'int',
+            'constraint' => 64,
+            'unsigned' => TRUE,
+            'auto_increment' => TRUE
+        ),
+        'image_id' => array(
+            'type' => 'int',
+            'constraint' => 64
+        ),
+        'type' => array(
+            'type' => 'varchar',
+            'constraint' => 64
+        ),
+        'src' => array(
+            'type' => 'varchar',
+            'constraint' => 2048
+        ),
+        'height' => array(
+            'type' => 'int',
+            'constraint' => 4
+        ),
+        'width' => array(
+            'type' => 'int',
+            'constraint' => 4
+        ),
+        'primary' => array(
+            'type' => 'bool'
+        )
+    );
+
+    protected $_keys = array(
+        'primary' => 'id',
+        'foreign' => 'image_id'
+    );
+
+    protected $_table_name = 'npr_story_api_stories_image_crops';
+}
