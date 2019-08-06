80e3723b83e97c281ed09e33c78d0767f47da1bb
Andrew Reynolds <areynold@illinois.edu>
2019-08-06 13:40:26 -0500

    stub story template plugin

---
+++ pi.npr_story_api.php
@@ -0,0 +1,13 @@
+<?php
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Npr_story_api_pi
+{
+    public $return_data;
+
+    public function __construct()
+    {
+        $this->return_data = "Hello NPR";
+    }
+}
