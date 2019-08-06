1a4e76de0df20d2a953d51e5bca7b1ceb9876973
Andrew Reynolds <areynold@illinois.edu>
2019-08-06 15:47:34 -0500

    use module file as plugin

--- mod.npr_story_api.php
+++ mod.npr_story_api.php
@@ -2,4 +2,26 @@
 class Npr_story_api 
 {
     public $return_data;
+
+    public function __construct()
+    {
+        $id = ee()->TMPL->fetch_param('id');
+        $story = $this->story($id);
+
+        $this->return_data = $story;
+    }
+
+    public function story($npr_story_id)
+    {
+        $model = ee('Model')->get('npr_story_api:Npr_story')
+            ->filter('id', $npr_story_id)
+            ->first();
+
+        if ($model === NULL)
+        {
+            $model = ee()->TMPL->no_results;
+        }
+
+        return $model;
+    }
 }
