f1b86bcdbbc01f0982d57a31d472ba56c7c695a1
Andrew Reynolds <areynold@illinois.edu>
2019-08-06 13:50:22 -0500

    create constants file

---
+++ constants.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit ('No direct script access allowed.');
+}
+
+class Constants
+{
+    const NAME = 'NPR Story API';
+
+    const AUTHOR = 'Illinois Public Media';
+
+    const AUTHOR_URL = 'https://will.illinois.edu';
+
+    const DESCRIPTION = "An ExpressionEngine port of NPR's story API Wordpress module (https://github.com/npr/nprapi-wordpress).";
+
+    const DOCS_URL = 'https://gitlab.engr.illinois.edu/willpublicmedia/npr_api_expressionengine';
+
+    const VERSION = '0.0.0';
+}
