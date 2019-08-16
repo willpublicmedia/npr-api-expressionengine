ea0cc96637618cead99ab0ef8c5d520737ae3d9b
Andrew Reynolds <areynold@illinois.edu>
2019-08-16 15:59:01 -0500

    require all table definitions

--- libraries/configuration/tables/table_loader.php
+++ libraries/configuration/tables/table_loader.php
@@ -10,6 +10,10 @@ require_once(__DIR__ . '/itable.php');
 use IllinoisPublicMedia\NprStoryApi\Libraries\Configuration\Tables\ITable;
 
 class Table_loader {
+    public function __construct() {
+        $this->preload_requirements(__DIR__);
+    }
+
     public function load(string $model_name): ITable {
         $table_name = 'IllinoisPublicMedia\\NprStoryApi\\Libraries\\Configuration\\Tables\\'
         . $model_name
@@ -17,5 +21,14 @@ class Table_loader {
 
         $data = new $table_name();
         return $data;
+    }
+
+    /** 
+     * Require all classes in the specified directory.
+     */
+    private function preload_requirements($preload_dir) {
+        foreach(glob(__DIR__ . '/*.php') as $file) {
+            require_once($file);
+        }
     }
 }
