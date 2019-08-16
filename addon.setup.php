3c88b3bdb2634feea4e19e8cfefb5bb277cf42d2
Andrew Reynolds <areynold@illinois.edu>
2019-08-16 16:45:56 -0500

    require content models in addon setup

--- addon.setup.php
+++ addon.setup.php
@@ -1,5 +1,19 @@
 <?php
     require_once (__DIR__ . '/constants.php');
+    require_once (__DIR__ . '/libraries/model/content/npr_audio.php');
+    require_once (__DIR__ . '/libraries/model/content/npr_audio_format.php');
+    require_once (__DIR__ . '/libraries/model/content/npr_byline.php');
+    require_once (__DIR__ . '/libraries/model/content/npr_html_asset.php');
+    require_once (__DIR__ . '/libraries/model/content/npr_image.php');
+    require_once (__DIR__ . '/libraries/model/content/npr_image_crop.php');
+    require_once (__DIR__ . '/libraries/model/content/npr_permalink.php');
+    require_once (__DIR__ . '/libraries/model/content/npr_organization.php');
+    require_once (__DIR__ . '/libraries/model/content/npr_pull_correction.php');
+    require_once (__DIR__ . '/libraries/model/content/npr_pull_quote.php');
+    require_once (__DIR__ . '/libraries/model/content/npr_related_link.php');
+    require_once (__DIR__ . '/libraries/model/content/npr_story.php');
+    require_once (__DIR__ . '/libraries/model/content/npr_text_paragraph.php');
+    require_once (__DIR__ . '/libraries/model/content/npr_thumbnail.php');
     use IllinoisPublicMedia\NprStoryApi\Constants;
 
     return array(
