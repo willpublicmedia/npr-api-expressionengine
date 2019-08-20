ce5eb053856d7dfee24f6e38f9e6320a6e5dab6f
Andrew Reynolds <areynold@illinois.edu>
2019-08-20 16:03:23 -0500

    define simple content fields

--- libraries/configuration/fields/story_content_definitions.php
+++ libraries/configuration/fields/story_content_definitions.php
@@ -8,5 +8,33 @@ if (!defined('BASEPATH')) {
 
 class Story_content_definitions
 {
-    public static $fields = array();
+    public static $fields = array(
+        'audio_runby_date' => array(
+            'field_name' => 'audio_runby_date',
+            'field_label' => 'Audio Run-By Date',
+            'field_instructions' => 'Date by which audio should be aired.',
+            'field_type' => 'date',
+            'field_list_items' => '',
+            'field_pre_populate' => 'n',
+            'field_pre_field_id' => 0,
+            'field_pre_channel_id' => 0,
+            'field_order' => 1
+        ),
+        'byline' => array(
+            'field_name' => 'byline',
+            'field_label' => 'Byline',
+            'field_instructions' => 'A comma-separated list of story contributors.',
+            'field_type' => 'text',
+            'field_maxl' => '',
+            'field_list_items' => '',
+            'field_pre_populate' => 'n',
+            'field_pre_field_id' => 0,
+            'field_pre_channel_id' => 0,
+            'field_order' => 1,
+            'field_settings' => array(
+                'field_fmt' => 'none',
+                'field_show_fmt' => 'n'
+            )
+        )
+    );
 }
