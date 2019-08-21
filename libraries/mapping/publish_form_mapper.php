a208413c9ec079d6f5146e2d8992eda777e13013
Andrew Reynolds <areynold@illinois.edu>
2019-08-21 16:11:48 -0500

    stub publish form mapper

---
+++ libraries/mapping/publish_form_mapper.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace IllinoisPublicMedia\NprStoryApi\Libraries\Mapping;
+
+if (!defined('BASEPATH'))
+{
+    exit ('No direct script access allowed.');
+}
+
+class Publish_form_mapper
+{
+}
