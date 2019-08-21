05d4ec83f13139f39f78553c44b30427df46de6e
Andrew Reynolds <areynold@illinois.edu>
2019-08-21 11:27:32 -0500

    add additional content field definitions

--- libraries/configuration/fields/story_content_definitions.php
+++ libraries/configuration/fields/story_content_definitions.php
@@ -36,25 +36,179 @@ class Story_content_definitions
                 'field_show_fmt' => 'n'
             )
         ),
+        'keywords' => array(
+            'field_name' => 'keywords',
+            'field_label' => 'Keywords',
+            'field_instructions' => 'A comma-delimited list of key terms describing the returned story. This field is seldom used for NPR.org.',
+            'field_type' => 'text',
+            'field_pre_populate' => 'n',
+            'field_list_items' => '',
+            'field_pre_field_id' => 0,
+            'field_pre_channel_id' => 0,
+            'field_order' => 1,
+            'field_settings' => array(
+                'field_fmt' => 'none',
+                'field_show_fmt' => 'n'
+            )
+        ),
+        'last_modified_date' => array(
+            'field_name' => 'last_modified_date',
+            'field_label' => 'Last Modified Date',
+            'field_instructions' => 'Date that a pulled story was last modified.',
+            'field_type' => 'date',
+            'field_list_items' => '',
+            'field_pre_field_id' => 0,
+            'field_pre_channel_id' => 0,
+            'field_order' => 1
+        ),
+        'mini_teaser' => array(
+            'field_name' => 'mini_teaser',
+            'field_label' => 'Mini Teaser',
+            'field_instructions' => 'An abbreviated abstract for the returned story, describing what the story is about.',
+            'field_type' => 'text',
+            'field_list_items' => '',
+            'field_pre_populate' => 'n',
+            'field_pre_field_id' => 0,
+            'field_pre_channel_id' => 0,
+            'field_order' => 1,
+            'field_settings' => array(
+                'field_fmt' => 'none',
+                'field_show_fmt' => 'n'
+            )
+        ),
+        'permalink' => array(
+            'field_name' => 'permalink',
+            'field_label' => 'Permalink',
+            'field_instructions' => "Permanent link to story on the story's original website",
+            'field_type' => 'text',
+            'field_list_items' => '',
+            'field_pre_populate' => 'n',
+            'field_pre_field_id' => 0,
+            'field_pre_channel_id' => 0,
+            'field_order' => 1,
+            'field_settings' => array(
+                'field_fmt' => 'none',
+                'field_show_fmt' => 'n'
+            )
+        ),
+        'priority_keywords' => array(
+            'field_name' => 'priority_keywords',
+            'field_label' => 'Priority Keywords',
+            'field_instructions' => 'A comma-delimited list of key terms that are very closely tied to the returned story.',
+            'field_type' => 'text',
+            'field_pre_populate' => 'n',
+            'field_list_items' => '',
+            'field_pre_field_id' => 0,
+            'field_pre_channel_id' => 0,
+            'field_order' => 1,
+            'field_settings' => array(
+                'field_fmt' => 'none',
+                'field_show_fmt' => 'n'
+            )
+        ),
+        'short_title' => array(
+            'field_name' => 'short_title',
+            'field_label' => 'Short Title',
+            'field_instructions' => 'An abbreviated title, not to exceed 30 characters.',
+            'field_type' => 'text',
+            'field_maxl' => '30',
+            'field_list_items' => '',
+            'field_pre_populate' => 'n',
+            'field_pre_field_id' => 0,
+            'field_pre_channel_id' => 0,
+            'field_order' => 1,
+            'field_settings' => array(
+                'field_fmt' => 'none',
+                'field_show_fmt' => 'n'
+            )
+        ),
+        'pub_date' => array(
+            'field_name' => 'pub_date',
+            'field_label' => 'Publication Date',
+            'field_instructions' => 'Date that a pulled story was originally published.',
+            'field_type' => 'date',
+            'field_list_items' => '',
+            'field_pre_field_id' => 0,
+            'field_pre_channel_id' => 0,
+            'field_order' => 1
+        ),
+        'slug' => array(
+            'field_name' => 'slug',
+            'field_label' => 'Slug',
+            'field_instructions' => "The main association for story, whether it is to a topic, series, column or some other list in the system.",
+            'field_type' => 'text',
+            'field_maxl' => '128',
+            'field_list_items' => '',
+            'field_pre_populate' => 'n',
+            'field_pre_field_id' => 0,
+            'field_pre_channel_id' => 0,
+            'field_order' => 1,
+            'field_settings' => array(
+                'field_fmt' => 'none',
+                'field_show_fmt' => 'n'
+            )
+        ),
+        'subtitle' => array(
+            'field_name' => 'subtitle',
+            'field_label' => 'Subtitle',
+            'field_instructions' => 'A short, sentence-like description of the story.',
+            'field_type' => 'text',
+            'field_maxl' => '',
+            'field_list_items' => '',
+            'field_pre_populate' => 'n',
+            'field_pre_field_id' => 0,
+            'field_pre_channel_id' => 0,
+            'field_order' => 1,
+            'field_settings' => array(
+                'field_fmt' => 'none',
+                'field_show_fmt' => 'n'
+            )
+        ),
+        'story_date' => array(
+            'field_name' => 'story_date',
+            'field_label' => 'Story Date',
+            'field_instructions' => "The primary date/time associated with the story's publication to NPR.org.",
+            'field_type' => 'date',
+            'field_list_items' => '',
+            'field_pre_field_id' => 0,
+            'field_pre_channel_id' => 0,
+            'field_order' => 1
+        ),
+        'teaser' => array(
+            'field_name' => 'teaser',
+            'field_label' => 'Teaser',
+            'field_instructions' => 'The main abstract for the returned story, describing what the story is about.',
+            'field_type' => 'textarea',
+            'field_maxl' => '',
+            'field_list_items' => '',
+            'field_pre_populate' => 'n',
+            'field_pre_field_id' => 0,
+            'field_pre_channel_id' => 0,
+            'field_order' => 1,
+            'field_settings' => array(
+                'field_fmt' => 'none',
+                'field_show_fmt' => 'n'
+            )
+        ),
+        'text' => array(
+            'field_name' => 'text',
+            'field_label' => 'Text',
+            'field_instructions' => 'The full text of the story, complete with markup.',
+            'field_type' => 'rte',
+            'field_maxl' => '',
+            'field_list_items' => '',
+            'field_pre_populate' => 'n',
+            'field_pre_field_id' => 0,
+            'field_pre_channel_id' => 0,
+            'field_order' => 1
+        ),
         /**
          *  'npr_audio' => $audio_array --> fluid
          *  'npr_corrections' => $corrections,
          *   'npr_html_assets' => $html_assets,
          *   'npr_images' => $images --> grid? model after channel images
-         *   'npr_keywords' => $story->keywords --> text
-         *   'npr_lastModifiedDate' => $story->lastModifiedDate --> date
-         *   'npr_miniTeaser' => $story->miniTeaser --> text/text-area
          *   'npr_organization' => $org_array
-         *   'npr_permalink' => $permalink --> url
-         *   'npr_priorityKeywords' => $story->priorityKeywords --> text
-         *   'npr_pubDate' => $story->pubDate --> date
          *   'npr_pullquotes' => $pullquotes
-         *   'npr_shortTitle' => $story->shortTitle --> text
-         *   'npr_slug' => $story->slug --> text
-         *   'npr_subtitle' => $story->subtitle --> text
-         *   'npr_storyDate' => $story->storyDate --> date
-         *   'npr_teaser' => $story->teaser --> textarea
-         *   'npr_text' => $text --> textarea
          *   'npr_thumbnails' => $thumbnail_array --> see images
          *   'npr_toenails' => $toenail_array --> see images
          */
