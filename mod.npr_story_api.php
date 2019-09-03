168f9a73c57f86aacf0c1d24673c2515b363f37c
Andrew Reynolds <areynold@illinois.edu>
2019-09-03 10:08:38 -0500

    remove template mapping method

--- mod.npr_story_api.php
+++ mod.npr_story_api.php
@@ -1,48 +1,10 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once(__DIR__ . '/libraries/mapping/template_mapper.php');
-use IllinoisPublicMedia\NprStoryApi\Libraries\Mapping\Template_mapper;
-
 class Npr_story_api 
 {
     public $return_data;
 
     public function __construct()
     {
-        $id = $this->validate_parameter(ee()->TMPL->fetch_param('npr_story_id'));
-        $tagdata = ee()->TMPL->tagdata;
-
-        $story = $this->story(intval($id));
-        $data = $this->process_story($story);
-        $variables = ee()->TMPL->parse_variables($tagdata, array($data));
-
-        $this->return_data = $variables;
-    }
-
-    public function story($npr_story_id)
-    {
-        $model = ee('Model')->get('npr_story_api:Npr_story')
-            ->filter('id', $npr_story_id)
-            ->first();
-
-        if ($model === NULL)
-        {
-            $model = ee()->TMPL->no_results;
-        }
-
-        return $model;
-    }
-
-    private function process_story($story)
-    {
-        $mapper = new Template_mapper();
-        $data = $mapper->map($story);
-
-        return $data;
-    }
-
-    private function validate_parameter($input)
-    {
-        return strip_tags($input);
     }
 }
