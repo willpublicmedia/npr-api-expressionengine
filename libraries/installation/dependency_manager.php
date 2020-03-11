eee03f5ffa1886559ae2ee92e9f4c72e3f062609
Andrew Reynolds <areynold@illinois.edu>
2020-03-11 12:14:20 -0500

    stub dependency manager

---
+++ libraries/installation/dependency_manager.php
@@ -0,0 +1,14 @@
+<?php
+namespace IllinoisPublicMedia\NprStoryApi\Libraries\Installation;
+
+if (!defined('BASEPATH')) {
+    exit ('No direct script access allowed.');
+}
+
+class Dependency_manager
+{
+    public function check_dependencies()
+    {
+        return false;
+    }
+}
