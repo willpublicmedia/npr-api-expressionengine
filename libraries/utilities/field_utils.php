edd500d77a9b78f1211b2328022278af0e12c565
Andrew Reynolds <areynold@illinois.edu>
2020-03-18 14:25:46 -0500

    stub field utility class

---
+++ libraries/utilities/field_utils.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace IllinoisPublicMedia\NprStoryApi\Libraries\Utilities;
+
+if (!defined('BASEPATH')) {
+    exit ('No direct script access allowed.');
+}
+
+class Field_utils
+{
+    public function get_field_id($name)
+    {
+        $field_id = ee('Model')->get('ChannelField')
+            ->filter('field_name', $name)
+            ->fields('field_id')
+            ->first()
+            ->field_id;
+        
+        return $field_id;
+    }
+
+    public function get_field_name($name)
+    {
+        $field = ee('Model')->get('ChannelField')
+            ->filter('field_name', $name)
+            ->first();
+
+        if ($field === NULL)
+        {
+            return '';
+        }
+
+        $field_id = $field->field_id;
+        $field_name = "field_id_{$field_id}";
+        
+        return $field_name;
+    }
+
+    public function get_grid_column_names($field_id)
+    {
+        $ids = ee()->grid_model->get_columns_for_field($field_id, 'channel');
+
+        $columns = array();
+        foreach ($ids as $id => $data)
+        {
+            $name = $data['col_name'];
+            $columns[$name] = "col_id_$id";
+        }
+
+        return $columns;
+    }
+}
