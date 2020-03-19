688465f56f1684ed6388e71a3222d76a30c5df66
Andrew Reynolds <areynold@illinois.edu>
2020-03-19 14:29:19 -0500

    gather required column, entry data

--- libraries/mapping/field_autofiller.php
+++ libraries/mapping/field_autofiller.php
@@ -21,7 +21,26 @@ class Field_autofiller
 
     public function autofill_audio($field_name, $entry)
     {
+        $field_id = $this->field_utils->get_field_id($field_name);
+        $column_names = $this->field_utils->get_grid_column_names($field_id);
         $audio_data = $this->field_utils->get_grid_values($entry, $field_name);
+
+        foreach ($audio_data as $item)
+        {
+            $audio_data['audio_type'] = '';
+            $audio_data['audio_duration'] = '';
+            $audio_data['audio_filesize'] = '';
+            $audio_data['audio_description'] = '';
+            $audio_data['audio_format'] = '';
+            $audio_data['audio_url'] = '';
+            $audio_data['audio_rights'] = '';
+            $audio_data['audio_permissions'] = '';
+            $audio_data['audio_title'] = '';
+            $audio_data['audio_region'] = '';
+            $audio_data['audio_rightsholder'] = '';
+        }
+
+        
         throw new \Exception('not implemented');
     }
 
