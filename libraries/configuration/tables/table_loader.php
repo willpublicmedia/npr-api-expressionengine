b82d11807e62527a8100d4b415355792721cd4f3
Andrew Reynolds <areynold@illinois.edu>
2021-08-27 12:48:58 -0500

    add optional params to loader->load method

--- libraries/configuration/tables/table_loader.php
+++ libraries/configuration/tables/table_loader.php
@@ -3,36 +3,41 @@
 namespace IllinoisPublicMedia\NprStoryApi\Libraries\Configuration\Tables;
 
 if (!defined('BASEPATH')) {
-    exit ('No direct script access allowed.');
+    exit('No direct script access allowed.');
 }
 
-require_once(__DIR__ . '/../../utilities/autoloader.php');
-require_once(__DIR__ . '/itable.php');
-use IllinoisPublicMedia\NprStoryApi\Libraries\Utilities\Autoloader;
+require_once __DIR__ . '/../../utilities/autoloader.php';
+require_once __DIR__ . '/itable.php';
 use IllinoisPublicMedia\NprStoryApi\Libraries\Configuration\Tables\ITable;
+use IllinoisPublicMedia\NprStoryApi\Libraries\Utilities\Autoloader;
 
-class Table_loader {
+class Table_loader
+{
     /**
      * @param $path Path to check for loadable files.
      */
-    public function __construct(string $path = '') {
+    public function __construct(string $path = '')
+    {
         $path = $path === '' ? __DIR__ : $path;
         $this->preload_requirements($path);
     }
 
-    public function load(string $model_name): ITable {
-        $table_name = 'IllinoisPublicMedia\\NprStoryApi\\Libraries\\Configuration\\Tables\\'
-        . $model_name
-        . '_table';
+    public function load(
+        string $model_name,
+        string $namespace = 'IllinoisPublicMedia\\NprStoryApi\\Libraries\\Configuration\\Tables\\',
+        string $identifier = '_table'
+    ): ITable {
+        $table_name = $namespace . $model_name . $identifier;
 
         $data = new $table_name();
         return $data;
     }
 
-    /** 
+    /**
      * Require all classes in the specified directory.
      */
-    private function preload_requirements($preload_dir) {
+    private function preload_requirements($preload_dir)
+    {
         $autoloader = new Autoloader();
         $autoloader->load_dir($preload_dir);
     }
