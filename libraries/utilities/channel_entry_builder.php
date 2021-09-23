deb51c9c85f233bb3ba4a47ff2af0b397239f109
Andrew Reynolds <areynold@illinois.edu>
2021-09-23 09:16:15 -0500

    stub channel entry builder

---
+++ libraries/utilities/channel_entry_builder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace IllinoisPublicMedia\NprStoryApi\Libraries\Utilities;
+
+if (!defined('BASEPATH')) {
+    exit ('No direct script access allowed.');
+}
+
+class Channel_entry_builder
+{
+}
