68201d8e486bd9361b34ca175fc583ee65cdaf7b
Andrew Reynolds <areynold@illinois.edu>
2022-10-25 17:08:41 -0500

    fetch field id, reference grid_model

--- libraries/installation/updates/updater_3_0_0.php
+++ libraries/installation/updates/updater_3_0_0.php
@@ -3,7 +3,6 @@
 namespace IllinoisPublicMedia\NprStoryApi\Libraries\Installation\Updates;
 
 use IllinoisPublicMedia\NprStoryApi\Libraries\Configuration\Fields\Story_content_definitions as Story_content_definitions;
-use IllinoisPublicMedia\NprStoryApi\Libraries\Installation\Field_installer;
 
 if (!defined('BASEPATH')) {
     exit('No direct script access allowed.');
@@ -22,22 +21,67 @@ class Updater_3_0_0
         return $success;
     }
 
+    private function add_grid_columns($definition, $field)
+    {
+        $grid_type = $definition['field_type'];
+        $settings = array(
+            'content_type' => 'channel',
+            'settings_form_field_name' => $grid_type,
+            'field_id' => $field->field_id,
+            'grid' => $definition['field_settings'][$grid_type],
+        );
+
+        $this->load_grid_lib($settings);
+        ee()->grid_lib->apply_settings($settings);
+    }
+
+    /**
+     * Loads Grid library and assigns relevant field information to it
+     */
+    private function load_grid_lib($settings)
+    {
+        // Loader strips leading slashes. Use path relative to Loader class.
+        ee()->load->library('../../ExpressionEngine/Addons/grid/libraries/Grid_lib.php');
+
+        // Attempt to get an entry ID first
+        $entry_id = (isset($settings['entry_id']))
+        ? $settings['entry_id'] :
+        ee()->input->get_post('entry_id');
+
+        // ee()->grid_lib->entry_id = ($this->content_id() == NULL) ? $entry_id : $this->content_id();
+        ee()->grid_lib->entry_id = $entry_id;
+        ee()->grid_lib->field_id = $settings['field_id'];
+        ee()->grid_lib->field_name = $settings['field_name'];
+        ee()->grid_lib->content_type = $settings['content_type'];
+        ee()->grid_lib->fluid_field_data_id = (isset($settings['fluid_field_data_id'])) ? $settings['fluid_field_data_id'] : 0;
+        ee()->grid_lib->in_modal_context = false;
+        ee()->grid_lib->settings_form_field_name = 'grid';
+    }
+
     private function remove_publish_form_columns(array $fields): bool
     {
         $success_audio_remove = false;
 
         $settings = [
-            'content' => [],
+            'defined' => [],
         ];
 
+        $this->load_grid_lib($settings);
+
         foreach ($fields as $field_name) {
-            $settings['content'][$field_name] = Story_content_definitions::$fields[$field_name];
+            $model = ee('Model')->get('ChannelField')->filter('field_name', $field_name)->first();
+
+            if ($model === null) {
+                continue;
+            }
+
+            $field_id = $model->field_id;
+            $columns = ee()->grid_model->get_columns_for_field($field_id, 'channel', false);
+            // $settings['defined'][$field_name] = Story_content_definitions::$fields[$field_name];
+            $definition = Story_content_definitions::$fields[$field_name];
         }
 
-        $installer = new Field_installer($settings);
-        $installer->install();
-
-        $this->log_message();
+        // ee()->grid_lib->apply_settings($settings);
 
         $success = $success_audio_remove;
 
